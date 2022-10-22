9e055e926acd38a3b049ee16038056a39b5e98a5
Eideann <aingeru.scotland@gmail.com>
2022-10-22 20:23:40 +0200

    Establecer estructura base para el proyecto.
    Ficheros/Clases y métodos propuestos en enunciado.

---
+++ src/main/java/AplicacionIMDB.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class AplicacionIMDB {
+
+    public static void main(String[] args){
+
+        //TO DO: ...
+
+        //TO DO: Cargar peliculas
+
+        //TO DO Cargar intirpretes
+
+
+        //Men�
+        Scanner sc = new Scanner(System.in);
+        int opcion=-1;
+
+        while(opcion!=0) {
+            System.out.println("Escoja una opcion:");
+            System.out.println("1. Mostrar informacion de pelicula");
+            System.out.println("2. Mostrar informacion de interprete");
+            System.out.println("3. A�adir voto a pelicula");
+
+            System.out.println("0. Salir");
+            opcion = Integer.parseInt(sc.nextLine());
+            switch(opcion) {
+                case 1:
+                    //TO DO
+                    break;
+                // TO DO
+            }
+        }
+        sc.close();
+    }
+
+}
