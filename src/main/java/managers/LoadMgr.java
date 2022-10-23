e4880cd0922f44aa543841530105842c2426a42f
Eideann <aingeru.scotland@gmail.com>
2022-10-23 20:07:37 +0200

    Preparación de nueva base (LoadMgr, SearchEngine y Data)

---
+++ src/main/java/managers/LoadMgr.java
@@ -0,0 +1,22 @@
+package managers;
+import modelos.CatalogoIMDB;
+import modelos.Pelicula;
+
+public class LoadMgr extends CatalogoIMDB {
+    public LoadMgr(){
+        super();
+    }
+
+    public void loadData(){
+        loadFilms();
+        loadActors();
+    }
+
+    private void loadFilms(){
+
+    }
+
+    private void loadActors(){
+
+    }
+}
---
+++ src/main/java/templates/Data.java
@@ -0,0 +1,22 @@
+package templates;
+
+import java.util.ArrayList;
+
+public class Data<T extends Comparable<T>> {
+    private ArrayList<T> listaGenerica;
+
+    public Data(){
+        listaGenerica = new ArrayList<>();
+    }
+
+    public void add(T elemento){
+        listaGenerica.add(elemento);
+    }
+
+    /**
+     * Búsqueda dicotómica
+     * @param elemento
+     * @return
+     */
+    public T buscar(T elemento){ return null; }
+}
