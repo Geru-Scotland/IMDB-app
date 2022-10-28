04c56cfc34c7e153ef7bef8b2d2cd585f3e7b5a9
Eideann <aingeru.scotland@gmail.com>
2022-10-28 02:48:59 +0200

    Datawrapper initial test file with JUnit5.

---
+++ src/test/templates/DataWrapperTest.java
@@ -0,0 +1,41 @@
+package templates;
+
+import exceptions.LoadMgrException;
+import managers.CatalogIMDB;
+import managers.LoadMgr;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DataWrapperTest {
+
+    static CatalogIMDB cat;
+
+    @BeforeAll
+    static void setUp() {
+        cat = CatalogIMDB.getInstance();
+        try{
+            LoadMgr loadMgr = new LoadMgr();
+            loadMgr.loadData();
+        } catch(LoadMgrException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void getList() {
+    }
+
+    @Test
+    void search() {
+    }
+
+    @Test
+    void binarySearch() {
+    }
+}
